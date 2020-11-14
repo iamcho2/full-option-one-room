df8627dfdb06cffa2a695f0fdf0479365b6730ae
iamcho2 <thatgrass@naver.com>
2020-11-14 16:31:43 +0900

    [초이] install cookie-parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import morgan from "morgan";
 import express from "express";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 4000;
@@ -9,6 +10,7 @@ const PORT = 4000;
 app.use(morgan("dev"));
 app.use(helmet())
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
