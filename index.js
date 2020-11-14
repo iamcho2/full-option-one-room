56f0c5f0f31c24cc362d51c584f2aa4487a541b3
iamcho2 <thatgrass@naver.com>
2020-11-14 16:30:15 +0900

    [초이] install helmet

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import morgan from "morgan";
 import express from "express";
-const app = express();
 
+const app = express();
 const PORT = 4000;
+
+
+app.use(morgan("dev"));
+app.use(helmet())
 
 function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
