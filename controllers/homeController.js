10f36f38737e630631eab6300bdf7f538b10cff4
husk <68003176+JangHanjun@users.noreply.github.com>
2020-11-14 22:45:27 +0900

    ...

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,1 +1,5 @@
-export const home = (req, res) => res.render("home");
+import {images} from "../db"
+
+export const home = (req, res) => {
+    res.render("home", {images});    //home 탬플릿에 db images 전달
+};
